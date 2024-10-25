98d05eaeb6f57847d9efc3c761b537aa5e4fd7fc
Thiago Correa <thiagofarbo@gmail.com>
2024-10-24 21:35:03 -0300

    Created contract

--- contracts/MerdadoLibre.sol
+++ contracts/MerdadoLibre.sol
@@ -3,6 +3,98 @@ pragma solidity ^0.8.27;
 
 contract MercadoLivre{
 
-    string public name = "MercadoLivre"; 
+    address public owner;
+
+       struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }   
+
+    struct Order {
+        uint256 time;
+        Item item;
+    }
+
+    mapping(uint256 => Item) public items;
+    mapping(address => uint256) public orderCount;
+    mapping(address => mapping(uint256 => Order)) public orders;
+
+    event Buy(address buyer, uint256 orderId, uint256 itemId);
+    event List(string name, uint256 cost, uint256 quantity);
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+
+    function list(uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock) public onlyOwner{
+
+        // Create Item
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+
+        // Save item to blockchain
+        items[_id] = item;
+
+        emit List(_name, _cost, _stock);
+    }   
+
+    //Buy products
+
+    function buy(uint256 _id) public payable{
+
+        //Get item
+        Item memory item = items[_id];
+
+        //Require enough ether to buy item
+        require(msg.value >= item.cost);
+
+         //Require item in stock
+        require(item.stock > 0);
+
+        //Create an order
+        Order memory order = Order(block.timestamp, item);
+
+        //Add order for user
+        orderCount[msg.sender]++;
+
+        orders[msg.sender][orderCount[msg.sender]] = order;
+
+        //Subtrac order
+
+        items[_id].stock = item.stock - 1;
+
+        emit Buy(msg.sender, orderCount[msg.sender], item.id);
+
+    }
+
+    //Withdraw funds
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success);
+    }
 
 }
--- test/MercadoLivre.js
+++ test/MercadoLivre.js
@@ -1,14 +1,127 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require("chai")
 
-const tokens = (n) =>{
-    return ethers.utils.parseUnits(n.toString(), 'ether')
+const tokens = (n) => {
+  return ethers.parseUnits(n.toString(), 'ether')
 }
 
-describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+// Global constants for listing an item...
+const ID = 1
+const NAME = "Shoes"
+const CATEGORY = "Clothing"
+const IMAGE = "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg"
+const COST = tokens(1)
+const RATING = 4
+const STOCK = 5
 
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
-});
+describe("MercadoLivre", () => {
+  let mercadoLivre
+  let deployer, buyer
+
+  beforeEach(async () => {
+    // Setup accounts
+    [deployer, buyer] = await ethers.getSigners()
+
+    // Deploy contract
+    const MercadoLivre = await ethers.getContractFactory("MercadoLivre")
+    mercadoLivre = await MercadoLivre.deploy()
+  })
+
+  describe("Deployment", () => {
+    it("Sets the owner", async () => {
+      expect(await mercadoLivre.owner()).to.equal(deployer.address)
+    })
+  })
+
+  describe("Listing", () => {
+    let transaction
+
+    beforeEach(async () => {
+      // List a item
+      transaction = await mercadoLivre.connect(deployer).list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK)
+      await transaction.wait()
+    })
+
+    it("Returns item attributes", async () => {
+      const item = await mercadoLivre.items(ID)
+
+      expect(item.id).to.equal(ID)
+      expect(item.name).to.equal(NAME)
+      expect(item.category).to.equal(CATEGORY)
+      expect(item.image).to.equal(IMAGE)
+      expect(item.cost).to.equal(COST)
+      expect(item.rating).to.equal(RATING)
+      expect(item.stock).to.equal(STOCK)
+    })
+
+    it("Emits List event", () => {
+      expect(transaction).to.emit(mercadoLivre, "List")
+    })
+  })
+
+  describe("Buying", () => {
+    let transaction
+
+    beforeEach(async () => {
+      // List a item
+      transaction = await mercadoLivre.connect(deployer).list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK)
+      await transaction.wait()
+
+      // Buy a item
+      transaction = await mercadoLivre.connect(buyer).buy(ID, { value: COST })
+      await transaction.wait()
+    })
+
+
+    it("Updates buyer's order count", async () => {
+      const result = await mercadoLivre.orderCount(buyer.address)
+      expect(result).to.equal(1)
+    })
+
+    it("Adds the order", async () => {
+      const order = await mercadoLivre.orders(buyer.address, 1)
+
+      expect(order.time).to.be.greaterThan(0)
+      expect(order.item.name).to.equal(NAME)
+    })
+
+    // it("Updates the contract balance", async () => {
+    //   const result = await ethers.provider.getBalance(mercadoLivre.address)
+    //   expect(result).to.equal(COST)
+    // })
+
+    it("Emits Buy event", () => {
+      expect(transaction).to.emit(mercadoLivre, "Buy")
+    })
+  })
+
+  describe("Withdrawing", () => {
+    let balanceBefore
+
+    beforeEach(async () => {
+      // List a item
+      let transaction = await mercadoLivre.connect(deployer).list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK)
+      await transaction.wait()
+
+      // Buy a item
+      transaction = await mercadoLivre.connect(buyer).buy(ID, { value: COST })
+      await transaction.wait()
+
+      // Get Deployer balance before
+      balanceBefore = await ethers.provider.getBalance(deployer.address)
+
+      // Withdraw
+      transaction = await mercadoLivre.connect(deployer).withdraw()
+      await transaction.wait()
+    })
+
+    it('Updates the owner balance', async () => {
+      const balanceAfter = await ethers.provider.getBalance(deployer.address)
+      expect(balanceAfter).to.be.greaterThan(balanceBefore)
+    })
+
+    // it('Updates the contract balance', async () => {
+    //   const result = await ethers.provider.getBalance(mercadoLivre.address)
+    //   expect(result).to.equal(0)
+    // })
+  })
+})
